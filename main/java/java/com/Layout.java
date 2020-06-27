0597ea275cb82c95d8610f3bcd7c5ee32496510c
Anna Sturgulewska <sturgulewskaanna@gmail.com>
2020-06-27 13:09:55 +0200

    kolejny fajny comit

--- main/java/java/com/Layout.java
+++ main/java/java/com/Layout.java
@@ -1,224 +1,239 @@
+import javafx.embed.swing.JFXPanel;
+
 import javax.swing.*;
+import javax.swing.table.TableColumnModel;
+import javax.swing.text.JTextComponent;
 import java.awt.*;
 
 public class Layout extends JFrame {
     public void startGui() {
-        JFrame oknoGłowne = new JFrame("System zarządzania turniejem");
+        JFrame window = new JFrame("System zarządzania turniejem");
         //oknoGłowne.setMinimumSize(new Dimension(10, 40));
         //oknoGłowne.setLocationRelativeTo(null);
-        oknoGłowne.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        oknoGłowne.setJMenuBar(addMenuBar());
+        window.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        window.setJMenuBar(addMenuBar());
         // oknoGłowne.add(addMainPanel());
-        oknoGłowne.setVisible(true);
-        oknoGłowne.pack();
+        window.setVisible(true);
+        window.pack();
     }
 
     private JMenuBar addMenuBar() {
-        JMenu druzynaMenu = new JMenu("Drużyna");
-        JMenu sedziowieMenu = new JMenu("Sędziowie");
-        JMenu turniejMenu = new JMenu("Turniej");
-        JMenu tablicaMenu = new JMenu("Tablica wyników");
+        JMenu groupMenu = new JMenu("Drużyna");
+        JMenu judgeMenu = new JMenu("Sędziowie");
+        JMenu tournamentMenu = new JMenu("Turniej");
+        JMenu tabelMenu = new JMenu("Tablica wyników");
 
         JMenuBar menuBar = new JMenuBar();
 
-        menuBar.add(druzynaMenu);
-        menuBar.add(sedziowieMenu);
-        menuBar.add(turniejMenu);
-        menuBar.add(tablicaMenu);
+        menuBar.add(groupMenu);
+        menuBar.add(judgeMenu);
+        menuBar.add(tournamentMenu);
+        menuBar.add(tabelMenu);
 
-        druzynaMenu.add(druzynaMenu());
-        sedziowieMenu.add(sedziowieMenu());
-        tablicaMenu.add(tablicaMenu());
-        turniejMenu.add(turniejMenu());
+        groupMenu.add(groupMenu());
+        judgeMenu.add(judgeMenu());
+        tabelMenu.add(tabelMenu());
+        tournamentMenu.add(tournamentMenu());
 
         return menuBar;
     }
 
     private Component addMainPanel() {
         JPanel panel = new JPanel();
-        GridBagLayout widok = new GridBagLayout();
-        panel.setLayout(widok);
+        GridBagLayout view = new GridBagLayout();
+        panel.setLayout(view);
         GridBagConstraints constr = new GridBagConstraints();
 
         constr.fill = GridBagConstraints.HORIZONTAL;
         constr.gridx = 0;
         constr.gridy = 1;
 
-        panel.add(druzynaMenu(), constr);
-        panel.add(sedziowieMenu(), constr);
-        panel.add(tablicaMenu(), constr);
-        panel.add(turniejMenu(), constr);
+        panel.add(groupMenu(), constr);
+        panel.add(judgeMenu(), constr);
+        panel.add(tabelMenu(), constr);
+        panel.add(tournamentMenu(), constr);
 
         return panel;
     }
 
-    private Component druzynaMenu() {
-        JPanel okno_druzyna = new JPanel(new GridBagLayout());
+    private Component groupMenu() {
+        JPanel window_group = new JPanel(new GridBagLayout());
         GridBagConstraints constraints = new GridBagConstraints();
 
-        JButton nowaDruzyna = new JButton("nowa drużyna");
-        JButton usunDruzyne = new JButton("usuń drużynę");
-        JButton zglosDruzyne = new JButton("zgłoś drużynę");
-        JButton wycofajDruzyne = new JButton("wycofaj drużynę");
-        JButton debugujDruzyne = new JButton("debuguj");
+        JButton new_group = new JButton("nowa drużyna");
+        JButton delete_group = new JButton("usuń drużynę");
+        JButton report_group = new JButton("zgłoś drużynę");
+        JButton withdraw_group = new JButton("wycofaj drużynę");
+        JButton debuguj_group = new JButton("debuguj");
 
 
-        JFormattedTextField pula = new JFormattedTextField("pula drużyny");
-        JFormattedTextField grajace = new JFormattedTextField("grające drużyny");
+        JFormattedTextField pole = new JFormattedTextField("pula drużyny");
+        JFormattedTextField play = new JFormattedTextField("grające drużyny");
 
 
         JTextArea textArea = new JTextArea(20, 20);
-        JScrollPane pole_1Druzyna = new JScrollPane(textArea);
-        JScrollPane pole_2Druzyna = new JScrollPane(textArea);
+        JScrollPane pole1_group = new JScrollPane(textArea);
+        JScrollPane pole_2group = new JScrollPane(textArea);
 
 
-        pole_1Druzyna.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-        pole_1Druzyna.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        pole1_group.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        pole1_group.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
-        pole_2Druzyna.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-        pole_2Druzyna.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        pole_2group.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        pole_2group.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
         constraints.insets = new Insets(30, 100, 30, 100);
         constraints.fill = GridBagConstraints.HORIZONTAL;
 
         constraints.gridy = 0;
         constraints.gridx = 0;
-        okno_druzyna.add(nowaDruzyna, constraints);
+        window_group.add(new_group, constraints);
 
         constraints.gridy = 1;
-        okno_druzyna.add(usunDruzyne, constraints);
+        window_group.add(delete_group, constraints);
 
         constraints.gridy = 2;
-        okno_druzyna.add(zglosDruzyne, constraints);
+        window_group.add(report_group, constraints);
 
         constraints.gridy = 3;
-        okno_druzyna.add(wycofajDruzyne, constraints);
+        window_group.add(withdraw_group, constraints);
 
         constraints.gridy = 4;
-        okno_druzyna.add(debugujDruzyne, constraints);
+        window_group.add(debuguj_group, constraints);
 
         constraints.gridy = 1;
         constraints.gridx = 3;
-        okno_druzyna.add(pula, constraints);
+        window_group.add(pole, constraints);
 
         constraints.gridy = 2;
         constraints.gridx = 3;
-        okno_druzyna.add(grajace, constraints);
+        window_group.add(play, constraints);
 
         constraints.gridx = 3;
         constraints.gridy = 2;
-        okno_druzyna.add(pole_1Druzyna, constraints);
+        window_group.add(pole1_group, constraints);
 
         constraints.gridx = 3;
         constraints.gridy = 2;
-        okno_druzyna.add(pole_2Druzyna, constraints);
+        window_group.add(pole_2group, constraints);
 
-        return okno_druzyna;
+        return window_group;
     }
 
-    private Component sedziowieMenu() {
-        JPanel okno_sedziowie = new JPanel(new GridBagLayout());
+    private Component judgeMenu() {
+        JPanel window_judge = new JPanel(new GridBagLayout());
         GridBagConstraints gridBagConstraints = new GridBagConstraints();
 
-        JButton dodajSedziego = new JButton("dodaj sędziego");
-        JButton usunSedziego = new JButton("usuń sędziego");
-        JButton modyfikujSedziego = new JButton("modyfikuj sędziego");
-        JButton debugujSedziego = new JButton("debuguj");
+        JButton addJudge = new JButton("dodaj sędziego");
+        JButton deleteJudge = new JButton("usuń sędziego");
+        JButton modifyJudge = new JButton("modyfikuj sędziego");
+        JButton debugujJudge= new JButton("debuguj");
 
         JFormattedTextField lista = new JFormattedTextField("lista dostepnych sędziów");
 
-        JScrollBar pole_1Sedzia = new JScrollBar();
-
+        JTextArea textArea = new JTextArea(20, 20);
+        JScrollPane pole_1Judge = new JScrollPane(textArea);
+        pole_1Judge.setSize(20, 30);
 
         gridBagConstraints.insets = new Insets(10, 100, 10, 100);
         gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
 
         gridBagConstraints.gridy = 0;
         gridBagConstraints.gridx = 0;
-        okno_sedziowie.add(dodajSedziego, gridBagConstraints);
+        window_judge.add(addJudge, gridBagConstraints);
 
         gridBagConstraints.gridy = 1;
-        okno_sedziowie.add(usunSedziego, gridBagConstraints);
+        window_judge.add(deleteJudge, gridBagConstraints);
 
         gridBagConstraints.gridy = 2;
-        okno_sedziowie.add(modyfikujSedziego, gridBagConstraints);
+        window_judge.add(modifyJudge, gridBagConstraints);
 
         gridBagConstraints.gridy = 3;
-        okno_sedziowie.add(debugujSedziego, gridBagConstraints);
+        window_judge.add(debugujJudge, gridBagConstraints);
 
         gridBagConstraints.gridy = 1;
         gridBagConstraints.gridx = 3;
-        okno_sedziowie.add(lista, gridBagConstraints);
+        window_judge.add(lista, gridBagConstraints);
 
-        gridBagConstraints.gridx = 4;
+        gridBagConstraints.gridx = 3;
         gridBagConstraints.gridy = 2;
-        okno_sedziowie.add(pole_1Sedzia, gridBagConstraints);
+        window_judge.add(pole_1Judge, gridBagConstraints);
 
-        return okno_sedziowie;
+        return window_judge;
     }
 
-    private Component tablicaMenu() {
-        JPanel okno_tablica = new JPanel(new GridBagLayout());
+    private Component tabelMenu() {
+        JPanel window_tabel = new JPanel();
         GridBagConstraints bagConstraints = new GridBagConstraints();
 
-        JFormattedTextField tablicaWynikow = new JFormattedTextField("tablica wyników");
+        JTextField score_board = new JTextField("tablica wyników");
 
-        JTable jTable = new JTable();
-        bagConstraints.gridy = 1;
-        bagConstraints.gridx = 3;
-        okno_tablica.add(tablicaWynikow, bagConstraints);
+        String[][] wiersze = {{"abc", "23"},{"cde", "111"}};
+        String[] kolumny = {"drużyna", "punktacja"};
 
-        bagConstraints.gridx = 4;
-        bagConstraints.gridy = 2;
-        okno_tablica.add(jTable, bagConstraints);
+        JTable jtable = new JTable(wiersze, kolumny);
+        jtable.setBounds(30, 40, 200, 300);
 
-        return okno_tablica;
+        JScrollPane sp = new JScrollPane(jtable);
+        window_tabel.add(sp);
 
+        window_tabel.setSize(500, 200);
+        window_tabel.setVisible(true);
+
+        bagConstraints.insets = new Insets(30, 100, 30, 100);
+        bagConstraints.fill = GridBagConstraints.HORIZONTAL;
+
+        bagConstraints.gridx = 1;
+        bagConstraints.gridy = 0;
+        window_tabel.add(score_board, bagConstraints);
+
+        bagConstraints.gridx = 1;
+        bagConstraints.gridy = 0;
+        window_tabel.add(jtable, bagConstraints);
+
+        return window_tabel;
     }
 
-    private Component turniejMenu() {
+    private Component tournamentMenu() {
 
-        JPanel okno_turniej = new JPanel(new GridBagLayout());
+        JPanel window_tournament = new JPanel(new GridBagLayout());
         GridBagConstraints contrs = new GridBagConstraints();
 
+        JRadioButton tournament = new JRadioButton("turniej siatkówki");
+        tournament.setSelected(true);
 
-        JRadioButton turniej = new JRadioButton("turniej siatkówki");
-        turniej.setSelected(true);
+        JButton modify_tournament = new JButton("modyfikuj mecz");
+        JButton generate_tournament = new JButton("generuj mecz");
+        JButton generate_finals = new JButton("generuj finały");
 
-
-        JButton modyfikujTurniej = new JButton("modyfikuj mecz");
-        JButton generujTurniej = new JButton("generuj mecz");
-        JButton generujFinaly = new JButton("generuj finały");
-
-        JFormattedTextField listaMeczow = new JFormattedTextField("lista meczów");
+        JFormattedTextField lista_match = new JFormattedTextField("lista meczów");
 
         JTable tablica = new JTable();
-
 
         contrs.insets = new Insets(10, 100, 10, 100);
         contrs.fill = GridBagConstraints.HORIZONTAL;
 
         contrs.gridy = 0;
         contrs.gridx = 0;
-        okno_turniej.add(turniej, contrs);
+        window_tournament.add(tournament, contrs);
 
         contrs.gridy = 1;
-        okno_turniej.add(modyfikujTurniej, contrs);
+        window_tournament.add(modify_tournament, contrs);
 
         contrs.gridy = 2;
-        okno_turniej.add(generujTurniej, contrs);
+        window_tournament.add(generate_tournament, contrs);
 
         contrs.gridy = 3;
-        okno_turniej.add(generujFinaly, contrs);
+        window_tournament.add(generate_finals, contrs);
 
         contrs.gridy = 1;
         contrs.gridx = 3;
-        okno_turniej.add(listaMeczow, contrs);
+        window_tournament.add(lista_match, contrs);
 
         contrs.gridx = 4;
         contrs.gridy = 2;
-        okno_turniej.add(tablica, contrs);
+        window_tournament.add(tablica, contrs);
 
-        return okno_turniej;
+        return window_tournament;
     }
 }
---
+++ main/java/java/com/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        new Layout().startGui();
+
+
+        //nawiązanie do hibernate podobnie jak bylo na ostatnich zajeciach
+        //start
+        //stop
+        //userInterfaceService
+        //zobaczListeDruzyn funkcjonalnosc
+        //zobaczTurieje funkcjonalnosc
+        //zobaczListeSedziow
+        //wszystkie funkcjonalnosci typu dodaj/usun ktore
+//czy to będzie okej??? :
+        /* GraJdbc connection = new GraJdbc();
+        connection.openConnetion();
+        connection.closeConnection();*/
+
+    }
+}
